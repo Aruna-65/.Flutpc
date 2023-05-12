15a5b9302803b028ed3bafd6f02297a193ee67d0
ARUNA <131846919+Aruna-65@users.noreply.github.com>
2023-05-12 12:42:27 +0530

    Add files via upload

---
+++ function.Dart.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("Add numbers");
+  int sum(int a, int b){
+    int result;
+    result = a+b;
+    return result;
+  }
+  var c = sum(45,12);
+  print("The sum of two numbers is: ${c}");
+}
