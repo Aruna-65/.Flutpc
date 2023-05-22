6960cffb29266d0f42d8066266c015b0c2e38a46
ARUNA <131846919+Aruna-65@users.noreply.github.com>
2023-05-22 17:07:54 +0530

    Add files via upload

---
+++ scaff.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(
+home:Scaffold(
+appBar:AppBar(
+title: Text('my first app'),
+  centerTitle: true,//title property
+),
+  body: Center(
+child: Text('Hello☺️!'),
+),
+  floatingActionButton:FloatingActionButton(onPressed: (){
+  },
+    child:Text('click'),
+  ),
+),
+));
+
+
+
+
