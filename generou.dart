29bc1c4bd08a2f65bfce9366fb8318f3c5f04408
ARUNA <131846919+Aruna-65@users.noreply.github.com>
2023-06-23 13:36:22 +0530

    Update generou.dart

--- generou.dart
+++ generou.dart
@@ -1,53 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_searchable_dropdown/flutter_searchable_dropdown.dart';
+
+class MyScreen extends StatefulWidget {
+  @override
+  _MyScreenState createState() => _MyScreenState();
+}
+
+class _MyScreenState extends State<MyScreen> {
+  String selectedDropdownValue = '';
+  List<String> dropdownValues = ['Option 1', 'Option 2', 'Option 3'];
+
+  bool checkboxValue = false;
+
+  TextEditingController textFieldController = TextEditingController();
+
+  String selectedSearchableDropdownValue = '';
+  List<DropdownMenuItem> searchableDropdownItems = [
+    DropdownMenuItem(child: Text('Item 1'), value: 'Item 1'),
+    DropdownMenuItem(child: Text('Item 2'), value: 'Item 2'),
+    DropdownMenuItem(child: Text('Item 3'), value: 'Item 3'),
+  ];
+
+  DateTime selectedDate;
+
+  @override
+  void dispose() {
+    textFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Screen'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              DropdownButtonFormField<String>(
+                value: selectedDropdownValue,
+                items: dropdownValues.map((value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedDropdownValue = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Dropdown',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              CheckboxListTile(
+                value: checkboxValue,
+                onChanged: (value) {
+                  setState(() {
+                    checkboxValue = value;
+                  });
+                },
+                title: Text('Checkbox'),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: textFieldController,
+                decoration: InputDecoration(
+                  labelText: 'Text Field',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              SearchableDropdown.single(
+                items: searchableDropdownItems,
+                value: selectedSearchableDropdownValue,
+                hint: 'Select an item',
+                searchHint: 'Search for an item',
+                onChanged: (value) {
+                  setState(() {
+                    selectedSearchableDropdownValue = value;
+                  });
+                },
+                isExpanded: true,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  final DateTime picked = await showDatePicker(
+                    context: context,
+                    initialDate: selectedDate ?? DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2025),
+                  );
+                  if (picked != null) {
+                    setState(() {
+                      selectedDate = picked;
+                    });
+                  }
+                },
+                child: Text(selectedDate != null
+                    ? 'Selected Date: ${selectedDate.toString()}'
+                    : 'Select Date'),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                height: 200.0,
+                child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Item $index'),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: (settings) {
-        // Handle route generation here
-        if (settings.name == '/home') {
-          return MaterialPageRoute(builder: (_) => HomeScreen());
-        } else if (settings.name == '/profile') {
-          return MaterialPageRoute(builder: (_) => ProfileScreen());
-        }
-        // Handle unknown routes or errors
-        return MaterialPageRoute(builder: (_) => ErrorScreen());
-      },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(child: Text('Home Screen')),
-    );
-  }
-}
-
-class ProfileScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Profile')),
-      body: Center(child: Text('Profile Screen')),
-    );
-  }
-}
-
-class ErrorScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Error')),
-      body: Center(child: Text('Error Screen')),
-    );
-  }
+  runApp(MaterialApp(
+    home: MyScreen(),
+  ));
 }
